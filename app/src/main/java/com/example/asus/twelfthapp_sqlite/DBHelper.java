ed44acf5ff8bf56e38a0b5e3b684e40770621be3
Sergii Shevchenko <zoomx3m@gmail.com>
2016-03-28 10:12:59 +0300

    initital commit (2 line work)

---
+++ app/src/main/java/com/example/asus/twelfthapp_sqlite/DBHelper.java
@@ -0,0 +1,31 @@
+package com.example.asus.twelfthapp_sqlite;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    public DBHelper(Context context) {
+        // конструктор суперкласса
+        super(context, "myDB", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        Log.d("LOG_TAG,", "--- onCreate database ---");
+        // создаем таблицу с полями
+        db.execSQL(" create table mytable ( "
+                + " id integer primary key autoincrement, "
+                + " name text, "
+                + " email text, "
+//                + " login text, "
+                + ");");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
---
+++ app/src/main/java/com/example/asus/twelfthapp_sqlite/MainActivity.java
@@ -0,0 +1,135 @@
+package com.example.asus.twelfthapp_sqlite;
+
+import android.app.Activity;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    final String LOG_TAG = "myLogs";
+
+    private Button btnAdd, btnRead, btnClear;
+    private EditText etName, etLastName, etEmail, etLogin, etPass;
+
+    DBHelper dbHelper;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initElements();
+
+//        btnAdd = (Button) findViewById(R.id.btnAdd);
+//        btnAdd.setOnClickListener(this);
+//
+//        btnRead = (Button) findViewById(R.id.btnRead);
+//        btnRead.setOnClickListener(this);
+//
+//        btnClear = (Button) findViewById(R.id.btnClear);
+//        btnClear.setOnClickListener(this);
+//
+//        etName = (EditText) findViewById(R.id.etName);
+//        etEmail = (EditText) findViewById(R.id.etEmail);
+//
+//        // создаем объект для создания и управления версиями БД
+//        dbHelper = new DBHelper(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        // создаем объект для данных
+        ContentValues cv = new ContentValues();
+
+        // получаем данные из полей ввода
+        String name = etName.getText().toString();
+        String email = etEmail.getText().toString();
+//        String login = etLogin.getText().toString();
+
+        // подключаемся к БД
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+
+        switch (v.getId()) {
+            case R.id.btnAdd:
+                Log.d(LOG_TAG, "--- Insert in mytable: ---");
+                // подготовим данные для вставки в виде пар: наименование столбца - значение
+
+                cv.put("name", name);
+                cv.put("email", email);
+//                cv.put("login", login);
+                // вставляем запись и получаем ее ID
+                long rowID = db.insert("mytable", null, cv);
+                Log.d(LOG_TAG, "row inserted, ID = " + rowID);
+                break;
+            case R.id.btnRead:
+                Log.d(LOG_TAG, "--- Rows in mytable: ---");
+                // делаем запрос всех данных из таблицы mytable, получаем Cursor
+                Cursor c = db.query("mytable", null, null, null, null, null, null, null);
+
+                // ставим позицию курсора на первую строку выборки
+                // если в выборке нет строк, вернется false
+                if (c.moveToFirst()) {
+
+                    // определяем номера столбцов по имени в выборке
+                    int idColIndex = c.getColumnIndex("id");
+                    int nameColIndex = c.getColumnIndex("name");
+                    int emailColIndex = c.getColumnIndex("email");
+//                    int loginColIndex = c.getColumnIndex("login");
+
+                    do {
+                        // получаем значения по номерам столбцов и пишем все в лог
+                        Log.d(LOG_TAG,
+                                "ID = " + c.getInt(idColIndex) +
+                                        ", name = " + c.getString(nameColIndex) +
+                                        ", email = " + c.getString(emailColIndex)
+//                                        ", login = " + c.getString(loginColIndex)
+                                         );
+                        // переход на следующую строку
+                        // а если следующей нет (текущая - последняя), то false - выходим из цикла
+                    } while (c.moveToNext());
+                } else
+                    Log.d(LOG_TAG, "0 rows");
+                c.close();
+                break;
+            case R.id.btnClear:
+                Log.d(LOG_TAG, "--- Clear mytable: ---");
+                // удаляем все записи
+                int clearCount = db.delete("mytable", null, null);
+                Log.d(LOG_TAG, "deleted rows count = " + clearCount);
+                break;
+        }
+        // закрываем подключение к БД
+        dbHelper.close();
+    }
+
+    private void initElements () {
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnAdd.setOnClickListener(this);
+
+        btnRead = (Button) findViewById(R.id.btnRead);
+        btnRead.setOnClickListener(this);
+
+        btnClear = (Button) findViewById(R.id.btnClear);
+        btnClear.setOnClickListener(this);
+
+        etName = (EditText) findViewById(R.id.etName);
+          //додаткові елементи
+//          etLastName = (EditText) findViewById(R.id.etLastName);
+        etEmail = (EditText) findViewById(R.id.etEmail);
+//          etLogin = (EditText) findViewById(R.id.etLogin);
+//          etPass = (EditText) findViewById(R.id.etPass);
+
+        // создаем объект для создания и управления версиями БД
+        dbHelper = new DBHelper(this);
+    }
+
+
+}
